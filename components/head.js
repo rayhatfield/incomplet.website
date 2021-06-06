f7e04a772ce152c98e08dd10dffda7b21c9f63b4
Ray Hatfield <ray.hatfield@gmail.com>
2021-06-06 00:44:43 -0500

    fix dom attributes for react

--- components/head.js
+++ components/head.js
@@ -8,8 +8,8 @@ export function Head() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.0.0/modern-normalize.min.css"
         integrity="sha512-ISS7cAi1PEhQ8jnbJpJZMd29NlhNj4AWYyLOSp2CE/CsHxTCu+r+t0D2yoJudVrd0/8fTVPUVDzY5Tvli75u/g=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
       />
     </H>
   );
