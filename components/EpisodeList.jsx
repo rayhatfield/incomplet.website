ffc8ce4b2296581b7815b63349adddcc6a5753d1
Ray Hatfield <ray.hatfield@gmail.com>
2021-06-17 23:49:08 -0500

    mobile menu tinkering

--- components/EpisodeList.jsx
+++ components/EpisodeList.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
@@ -7,28 +8,51 @@ import { getEpisodes } from "../lib/episodes";
 
 import styles from "./EpisodeList.module.css";
 
+const MENU = "menu";
+
 function Episode({
   episode: { title, image_url, slug } = {},
   active,
   ...other
 }) {
+  const Cmp = active
+    ? (props) => <label htmlFor={MENU} {...props} />
+    : (props) => <Link href={`/episodes/${slug}`} {...props} />;
   return (
-    <Link href={`/episodes/${slug}`}>
+    <Cmp>
       <a className={clsx(styles.episode, { [styles.active]: active })}>
         {title}
       </a>
-    </Link>
+    </Cmp>
   );
 }
 
 export default function Episodes({ episodes, active }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const closeMenu = useCallback(() => setMenuOpen(false));
+
   return (
-    <ol className={styles.list}>
-      {episodes.map((ep) => (
-        <li key={ep.title}>
-          <Episode episode={ep} active={ep.id === active?.id} />
-        </li>
-      ))}
-    </ol>
+    <nav className={styles.container}>
+      <input
+        type="checkbox"
+        id={MENU}
+        checked={menuOpen}
+        onChange={(e) => setMenuOpen(e.target.checked)}
+      />
+      <ol className={styles.list}>
+        {episodes.map((ep) => {
+          const isActive = ep.id === active?.id;
+          return (
+            <li
+              key={ep.title}
+              className={clsx({ [styles.active]: isActive })}
+              onClick={closeMenu}
+            >
+              <Episode episode={ep} active={isActive} />
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
   );
 }
