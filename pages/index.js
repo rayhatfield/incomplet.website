3bd9a92e7a6be5bd0a2366ab3469c9c706ccb3cc
Ray Hatfield <ray.hatfield@gmail.com>
2021-06-23 21:57:32 -0500

    redirect home => latest episode

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import { Redirect } from "../components/redirect";
+import { getLatestEpisode } from "../lib/episodes";
 
-export default function IndexPage() {
-  return <Redirect to="/episodes" />;
+export default function IndexPage({ episode: { slug } = {} }) {
+  return <Redirect to={`/episodes/${slug}`} />;
+}
+
+export async function getStaticProps() {
+  const episode = await getLatestEpisode();
+  return {
+    props: {
+      episode,
+    },
+  };
 }
