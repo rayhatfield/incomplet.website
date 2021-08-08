6a6dde8f22c54675d762ed720b0464099dce54e8
Ray Hatfield <ray.hatfield@gmail.com>
2021-08-07 19:09:15 -0500

    update header, add logo

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { Head } from "./head";
 import styles from "./layout.module.css";
@@ -9,8 +10,21 @@ export function Layout({ children }) {
       <Head />
       <header className={styles.header}>
         <h1>
-          <Link href="/">
+          {/* <Link href="/">
             <a>incomplet</a>
+          </Link> */}
+          <Link href="/">
+            <div>
+              <img
+                src="/images/logo-w-tag.svg"
+                alt="incomplet: a podcast about design history"
+                className={styles["logo-main"]}
+                // width={336}
+                // height={336}
+                // objectFit="cover"
+              />
+              <p className={styles.tagline}>a podcast about design history</p>
+            </div>
           </Link>
         </h1>
         <nav className={styles.nav}>
